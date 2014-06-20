0e75797dc46ab610076a86128315ce54c91f95b4
Chris Adams <ca@pivotallabs.com>
2014-06-19 17:39:02 -0700

    Change runner to extend all timeouts to 90s
    This change also causes ginkgo to display when commands finish along with a report of how long they took. We did this to aid analysis of tests that were timing out and allow cats to pass as a purely functional test (with no performance testing aspect) on slower CF instances.
    
    Signed-off-by: Jesse Alford <jalford@pivotallabs.com>

--- Godeps/_workspace/src/github.com/pivotal-cf-experimental/cf-test-helpers/runner/run.go
+++ Godeps/_workspace/src/github.com/pivotal-cf-experimental/cf-test-helpers/runner/run.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/config"
@@ -18,19 +19,28 @@ var CommandInterceptor = func(cmd *exec.Cmd) *exec.Cmd {
 
 func Run(executable string, args ...string) *gexec.Session {
 	cmd := exec.Command(executable, args...)
+	started := time.Now()
+
+	startColor := ""
+	endColor := ""
+	if !config.DefaultReporterConfig.NoColor {
+		startColor = "\x1b[32m"
+		endColor = "\x1b[0m"
+	}
 
 	if config.DefaultReporterConfig.Verbose {
-		startColor := ""
-		endColor := ""
-		if !config.DefaultReporterConfig.NoColor {
-			startColor = "\x1b[32m"
-			endColor = "\x1b[0m"
-		}
 		fmt.Println("\n", startColor, "> ", strings.Join(cmd.Args, " "), endColor)
 	}
 
 	sess, err := gexec.Start(CommandInterceptor(cmd), ginkgo.GinkgoWriter, ginkgo.GinkgoWriter)
 	Expect(err).NotTo(HaveOccurred())
+
+	Eventually(sess, 90).Should(gexec.Exit())
+
+	if config.DefaultReporterConfig.Verbose {
+		duration := time.Since(started)
+		fmt.Println("\n", startColor, ">> ", strings.Join(cmd.Args, " "), "took", duration.Seconds(), "s", endColor)
+	}
 
 	return sess
 }
